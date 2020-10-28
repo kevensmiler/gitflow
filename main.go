2d123b4454332fdd0ef0c01d3c4141e589550ce1
keven <keven@163.com>
2020-10-28 16:41:38 +0800

    implement login module

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"gitflow/login"
+)
 
 func main() {
+	isSuccess := login.Login()
+	if isSuccess {
+		fmt.Println("login success!")
+	} else {
+		fmt.Println("login failed!")
+	}
 }
